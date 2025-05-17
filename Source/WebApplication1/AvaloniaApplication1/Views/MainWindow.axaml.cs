21b8a0283fe874d59937ab7103b5108ea7311d32
Thomas L <thomaslachin89@gmail.com>
2025-05-17 15:45:44 -0500

    Trying out avalonia

---
+++ Source/WebApplication1/AvaloniaApplication1/Views/MainWindow.axaml.cs
@@ -0,0 +1,18 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace AvaloniaApplication1.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    public void ClickHandler(object sender, RoutedEventArgs args)
+    {
+        Console.WriteLine("I've been clicked!");
+        buttonMessage.Text = "Button clicked!";
+    }
+}
