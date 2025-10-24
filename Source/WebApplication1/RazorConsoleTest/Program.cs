8d3c99b17415c8c7565a0460a0eb509dc93c26d9
Thomas L <thomaslachin89@gmail.com>
2025-10-24 11:05:34 -0500

    having fun trying razor console

---
+++ Source/WebApplication1/RazorConsoleTest/Program.cs
@@ -0,0 +1,10 @@
+﻿// Program.cs
+
+using Microsoft.Extensions.Hosting;
+using RazorConsole.Core;
+using RazorConsoleTest;
+
+IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args)
+    .UseRazorConsole<Counter>();
+IHost host = hostBuilder.Build();
+await host.RunAsync();
