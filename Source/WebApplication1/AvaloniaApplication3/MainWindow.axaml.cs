83806e404c5851f506cf6952a11e1b3f5ea0b2e3
Thomas L <thomaslachin89@gmail.com>
2026-03-11 21:52:05 -0500

    More tinkering

--- Source/WebApplication1/AvaloniaApplication3/MainWindow.axaml.cs
+++ Source/WebApplication1/AvaloniaApplication3/MainWindow.axaml.cs
@@ -17,7 +17,6 @@ public partial class MainWindow : Window
     public async void GetMainMonitorResolution(object sender, RoutedEventArgs args)
     {
         string resolution = $"{this.Screens.Primary.Bounds.Width}x{this.Screens.Primary.Bounds.Height}";
-        // await TopLevel.GetTopLevel(this).Clipboard.SetTextAsync(guid);
         tbResolution.Text = resolution;
 
         List<string> listOfActiveWindows = new();
@@ -27,13 +26,13 @@ public partial class MainWindow : Window
             .Where(p=> p.MainWindowHandle != 0)
             .ToArray();
         
-        listOfActiveWindows.AddRange(processesWithWindows.Select(_=>_.ProcessName).OrderBy(_=>_));
-        GetActiveWindows();
+        listOfActiveWindows.AddRange(GetActiveWindows().Select(_=>_).OrderBy(_=>_));
         tbListOfActiveWindows.Text = string.Join('\r', listOfActiveWindows);
     }
 
-    public void GetActiveWindows()
+    public List<string> GetActiveWindows()
     {
+        List<string> processNames = new ();
         try
         {
             var startInfoHostName = new ProcessStartInfo
@@ -75,7 +74,17 @@ public partial class MainWindow : Window
                 if (!string.IsNullOrWhiteSpace(output))
                 {
                     var newLineList = output.Split('\n');
-                    Console.WriteLine($"{newLineList}");
+                    foreach (var line in newLineList)
+                    {
+                        if (!string.IsNullOrWhiteSpace(line))
+                        {
+                            // Get index of line after hostname until end of the line
+                            int startIndex = line.IndexOf(hostname) + hostname.Length;
+                            int endIndex = line.Length - startIndex;
+                            string process = line.Substring(startIndex, endIndex);
+                            processNames.Add(process);
+                        }
+                    }
                 }
             }
         }
@@ -84,5 +93,6 @@ public partial class MainWindow : Window
             // Handle exceptions (e.g., access denied or process without a window)
             Console.WriteLine($"Could not access process: {ex.Message}");
         }
+        return processNames;
     }
 }
